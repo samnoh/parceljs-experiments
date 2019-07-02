46876e170fab931a35d63041893ab4a8c8c24e69
Sam Noh <samnoh21@outlook.com>
2019-07-02 15:28:51 +1200

    add - babel-plugin-module-resolver

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import loadable from '@loadable/component';
 
-const Home = loadable(() => import('./Home'));
-const Counter = loadable(() => import('./Counter'));
+const Home = loadable(() => import('Home'));
+const Counter = loadable(() => import('Counter/Counter'));
 
 const App = () => {
     return (
